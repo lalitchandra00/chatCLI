add556c7464d7b8dc7f9777c440841dc61ceabed
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-09-25 09:19:57 +0530

    phase 0

---
+++ chatbot.py
@@ -0,0 +1,24 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+HF_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN") or os.getenv("HUGGINGFACE_API_KEY")
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=HF_API_TOKEN,
+)
+
+model = ChatHuggingFace(llm=llm)
+
+while True:
+    user_input = input("You : ")
+    if user_input.strip().lower() == "exit":
+        break
+    result = model.invoke(user_input)
+    print("AI : ", result.content)   
+
+
